366df9ac2da0283de7c9674ba6696131c3e050b0
Vania <sonalysifuentes@gmail.com>
2025-05-10 19:44:52 -0500

    views created

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Tag
+from .serializers import CategorySerializer, TagSerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer

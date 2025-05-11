38481e2dd92a7857a304773373b9a0821c249233
Eduardo <eduardobullonvera@gmail.com>
2025-05-10 21:26:31 -0500

    Analytics Views

--- analytics/views.py
+++ analytics/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import QuestionStat, QuizActivity
+from .serializers import QuestionStatSerializer, QuizActivitySerializer
 
-# Create your views here.
+class QuestionStatViewSet(viewsets.ModelViewSet):
+    queryset = QuestionStat.objects.all()
+    serializer_class = QuestionStatSerializer
+
+class QuizActivityViewSet(viewsets.ModelViewSet):
+    queryset = QuizActivity.objects.all()
+    serializer_class = QuizActivitySerializer

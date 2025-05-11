c83432bd87edb4d88b79ff2a5371f909495e26c9
Vania <sonalysifuentes@gmail.com>
2025-05-10 19:56:09 -0500

    views updated

--- categories/views.py
+++ categories/views.py
@@ -1,12 +1,26 @@
 from rest_framework import viewsets
-from .models import Category, Tag
-from .serializers import CategorySerializer, TagSerializer
+from .models import Category, Tag, Quiz
+from .serializers import CategorySerializer, TagSerializer, QuizSerializer, QuizDetailSerializer
 
+# ViewSet para Categorías
 class CategoryViewSet(viewsets.ModelViewSet):
+    """ViewSet para listar, crear, actualizar y eliminar categorías"""
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-
+# ViewSet para Tags
 class TagViewSet(viewsets.ModelViewSet):
+    """ViewSet para listar, crear, actualizar y eliminar tags"""
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+
+# ViewSet para Quizzes
+class QuizViewSet(viewsets.ModelViewSet):
+    """ViewSet para listar, crear, actualizar y eliminar quizzes"""
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
+    
+    def get_serializer_class(self):
+        if self.action == 'retrieve':  # Si la acción es obtener detalles, usamos el serializer detallado
+            return QuizDetailSerializer
+        return self.serializer_class

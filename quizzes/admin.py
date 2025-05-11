232d4b601b2ee31c4b388553beaeff14354ebb30
abuenoh-tecsup <alvaro.bueno@tecsup.edu.pe>
2025-05-10 22:30:28 -0500

    Registration for admin

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from quizzes.models import Choice, Question, Quiz
+
 # Register your models here.
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Choice)
--- users/serializers.py
+++ users/serializers.py
@@ -1,21 +1,16 @@
+# users/serializers.py
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Profile, QuizAttempt
 from quizzes.serializers import QuizSerializer
 
 class ProfileSerializer(serializers.ModelSerializer):
-    """
-    Serializer para crear o actualizar el perfil de un usuario.
-    """
     class Meta:
         model = Profile
         fields = ['id', 'user', 'bio', 'avatar']
         read_only_fields = ['created_at']
 
 class ProfileDetailSerializer(serializers.ModelSerializer):
-    """
-    Serializer detallado para ver el perfil del usuario con detalles del usuario.
-    """
     user = serializers.StringRelatedField()
 
     class Meta:
@@ -24,18 +19,12 @@ class ProfileDetailSerializer(serializers.ModelSerializer):
 
 
 class QuizAttemptSerializer(serializers.ModelSerializer):
-    """
-    Serializer para crear o actualizar un intento de quiz (con score, max_score, etc.)
-    """
     class Meta:
         model = QuizAttempt
         fields = ['id', 'user', 'quiz', 'score', 'max_score']
         read_only_fields = ['completed_at']
 
 class QuizAttemptDetailSerializer(serializers.ModelSerializer):
-    """
-    Serializer detallado para ver un intento de quiz con detalles completos del usuario y quiz.
-    """
     user = serializers.StringRelatedField()
     quiz = QuizSerializer()
 

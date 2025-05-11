ba32e68fde38744b07a87bd82e72742b5d5ab157
Vania <sonalysifuentes@gmail.com>
2025-05-10 19:56:03 -0500

    urls updated

--- categories/urls.py
+++ categories/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, TagViewSet
+from .views import QuizViewSet, CategoryViewSet, TagViewSet
 
+# Crear el router y registrar los ViewSets
 router = DefaultRouter()
-router.register(r'categories', CategoryViewSet)
-router.register(r'tags', TagViewSet)
+router.register(r'quizzes', QuizViewSet)
+router.register(r'categories', CategoryViewSet)  # Rutas para Categorías
+router.register(r'tags', TagViewSet)  # Rutas para Tags
 
+# Incluir las rutas en el archivo principal de URLs
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
